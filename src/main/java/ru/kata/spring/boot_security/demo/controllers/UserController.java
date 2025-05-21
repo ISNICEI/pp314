45e0677f4dd63f384a025082e4e88fadc896bb18
ISNICEI <49389754+ISNICEI@users.noreply.github.com>
2025-05-21 11:43:09 +0300

    починил юзера

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -2,13 +2,15 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -19,8 +21,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String getUserProfile(Model model) {
+        model.addAttribute("user", userService.getCurrentUser());
+        return "oneUser";
+    }
     @GetMapping("/profile")
-    public User getUserProfile() {
+    @ResponseBody
+    public User getCurrentUserProfile() {
         return userService.getCurrentUser();
     }
 }
